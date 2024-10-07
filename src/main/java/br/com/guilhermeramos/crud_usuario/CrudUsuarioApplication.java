594d5e4f3efe7bb2ebac6968f4f004ce0fcba7ff
guirmsilva <devguilhermeramos@gmail.com>
2024-10-07 17:34:10 -0300

    first commit

--- src/main/java/br/com/guilhermeramos/crud_usuario/CrudUsuarioApplication.java
+++ src/main/java/br/com/guilhermeramos/crud_usuario/CrudUsuarioApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CrudUsuarioApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CrudUsuarioApplication.class, args);
 	}
